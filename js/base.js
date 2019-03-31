4c91b12f9040e5a0db395cadec06676c5716c3b5
CrazyQii <858590598@qq.com>
2019-03-31 11:38:15 +0800

    添加逻辑层

--- js/base.js
+++ js/base.js
@@ -1,12 +1,42 @@
-//获取收起目录的id
 window.onload = function() {
 
-    //初始化子目录隐藏
+    /*
+     *侧边栏
+     */
+    //初始化子目录隐藏,所有页面侧边栏
     index.style.display = "none";
     downLoad.style.display = "none";
     map.style.display = "none";
     car_list.style.display = "none";
+
+    /*
+     *报表下载页面
+     */
+    //动态按钮
+    var reportTime = document.getElementById('reportTime');
+    var button = reportTime.getElementsByTagName('button');
+    for (let i = 0; i < button.length; i++) {
+        button[i].id = "button" + i;
+    }
+    button[0].style.color = "#3da2ff";
+    button[0].style.borderColor = "#3da2ff";
+
+    //获取动态表格信息
+    var day = document.getElementById('day');
+    var month = document.getElementById('month');
+    var year = document.getElementById('year');
+    day.style.display = "table-row-group";
+    month.style.display = "none";
+    year.style.display = "none";
+    //获取切换表格信息
+    var change_day = document.getElementById('changeDay');
+    var change_month = document.getElementById('changeMonth');
+    var change_year = document.getElementById('changeYear');
+    change_day.style.display = "block";
+    change_month.style.display = "none";
+    change_year.style.display = "none";
 }
+
 
 /**
  * 函数名：getList()
